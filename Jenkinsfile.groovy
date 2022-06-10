7943f8844fa94a7587257477ea8b161bc0065626
alexandre.basilio <alexandre.basilio@kuehne-nagel.com>
2022-06-10 09:51:10 +0300

    Creating sonarqube and sonarqube_database - wip

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,32 @@
+pipeline {
+    agent any
+    stages {
+        stage('Checkout SCM') {
+            steps {
+                checkout([$class: 'GitSCM', branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/alexfbasa/pact-backend.git']]])
+                sh 'ls -la'
+                sh 'mvn clean package -DskipTests=true'
+            }
+        }
+        stage('Unit Tests') {
+            steps {
+                sh 'mvn test'
+            }
+        }
+        stage('Sonar Analysis') {
+            environment {
+                scannerHome = tool 'SONNAR_SCANNER'
+            }
+            steps {
+                withSonarQubeEnv('docker_sonar') {
+                    sh "${scannerHome}/bin/sonar-scanner -Dsonar.host.url=http://sonar:9000 -Dsonar.login=6284aea55a28f72b4b8833a9dfe72b0cf53467fa -Dsonar.java.binaries=target -Dsonar.projectKey=DeployBack"
+                }
+            }
+        }
+        stage('Quality Gate') {
+            timeout(time: 1, unit: 'MINUTES') {
+                waitForQualityGate: true
+            }
+        }
+    }
+}
